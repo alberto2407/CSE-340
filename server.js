8e2ff426e5542290ff1177e9475f5467bac5c920
alberto2407 <albertofc2407@gmail.com>
2025-11-17 21:43:03 +0100

    W03 Assignment: Vehicle Detail Page

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const e = require("express")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/");
+const errorRoute = require("./routes/errorRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -21,10 +26,37 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+// Serve static files from the public directory
+app.use(express.static("public")) 
 // Index Route
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
+app.get("/", utilities.handleErrors(baseController.buildHome))
+// Inventory routes
+app.use("/inv", inventoryRoute)
+// Error route
+app.use("/", errorRoute)
+// Management to avoid Chrome DevTools error
+app.get("/.well-known/appspecific/com.chrome.devtools.json", (req, res) => {
+  res.status(404).send("Not Found");
+});
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} 
+  else {message = '<p class="notice">Oh no! There was a crash. Maybe try a different route?</p>'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
 })
 
 /* ***********************
